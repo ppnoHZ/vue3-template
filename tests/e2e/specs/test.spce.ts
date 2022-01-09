1423eb0015f5a19fdf704c1937e581605fd4ab5f
zhoudd3 <ppnohz@outlook.com>
2022-01-09 14:40:10 +0800

    feat: 集成cypress

---
+++ tests/e2e/specs/test.spce.ts
@@ -0,0 +1,6 @@
+describe('first', () => {
+    it('should', () => {
+        cy.visit('http://localhost:3000/')
+        cy.get('button').click()
+    })
+})
--- tests/unit/index.spec.ts
+++ tests/unit/index.spec.ts
@@ -1,6 +1,6 @@
 import HelloWorld from '../../src/components/HelloWorld.vue'
 
-test("1+1", () => {
+test("1+1=2", () => {
     console.log(HelloWorld)
     expect(1 + 1).toBe(2)
 })
